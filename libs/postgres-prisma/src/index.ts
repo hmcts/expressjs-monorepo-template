910334fb9380305f521f5e8aab2a119825f1d1b5
Linus Norton <linusnorton@gmail.com>
2025-11-06 10:08:02 +0000

    lint

--- libs/postgres-prisma/src/index.ts
+++ libs/postgres-prisma/src/index.ts
@@ -1,16 +1,9 @@
 import { PrismaClient } from "@prisma/client";
 
 // Construct DATABASE_URL from individual env vars if available
-if (
-	process.env.POSTGRES_HOST &&
-	process.env.POSTGRES_USER &&
-	process.env.POSTGRES_PASSWORD &&
-	process.env.POSTGRES_PORT &&
-	process.env.POSTGRES_DATABASE
-) {
-	const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_DATABASE } =
-		process.env;
-	process.env.DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`;
+if (process.env.POSTGRES_HOST && process.env.POSTGRES_USER && process.env.POSTGRES_PASSWORD && process.env.POSTGRES_PORT && process.env.POSTGRES_DATABASE) {
+  const { POSTGRES_HOST, POSTGRES_USER, POSTGRES_PASSWORD, POSTGRES_PORT, POSTGRES_DATABASE } = process.env;
+  process.env.DATABASE_URL = `postgresql://${POSTGRES_USER}:${POSTGRES_PASSWORD}@${POSTGRES_HOST}:${POSTGRES_PORT}/${POSTGRES_DATABASE}`;
 }
 
 // Default to local PostgreSQL if DATABASE_URL is not set
