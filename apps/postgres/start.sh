34c28c7081aa06462ab096c3f761a86f7b0d1ff9
Linus Norton <linusnorton@gmail.com>
2025-11-06 13:15:47 +0000

    prisma studio fixes

--- apps/postgres/start.sh
+++ apps/postgres/start.sh
@@ -7,7 +7,7 @@ npx prisma migrate deploy --schema=./dist/schema.prisma
 if [ $? -eq 0 ]; then
   echo "Migrations completed successfully"
   echo "Starting Prisma Studio on port 5555..."
-  npx prisma studio --schema=./dist/schema.prisma --port 5555 --host 0.0.0.0
+  npx prisma studio --schema=./dist/schema.prisma --port 5555 --hostname 0.0.0.0 --browser none
 else
   echo "Migration failed, exiting..."
   exit 1
