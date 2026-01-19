79b30676a941a79aa47d3fc7f7f001f9967c5e23
Linus Norton <linusnorton@gmail.com>
2026-01-19 05:47:31 +0000

    Use data source for resource group (managed by separate pipeline)

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,9 +4,6 @@ provider "azurerm" {
   subscription_id = var.subscription
 }
 
-resource "azurerm_resource_group" "rg" {
-  name     = "${var.product}-${var.env}"
-  location = var.location
-
-  tags = var.common_tags
+data "azurerm_resource_group" "rg" {
+  name = "${var.product}-${var.env}"
 }
