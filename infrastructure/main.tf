aaf6f2ec3c74d1adea0d6f0957bf57f59315202b
Linus Norton <linusnorton@gmail.com>
2026-01-21 17:36:27 +0000

    Add postgres db

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -4,6 +4,12 @@ provider "azurerm" {
   subscription_id = var.subscription
 }
 
+provider "azurerm" {
+  alias           = "postgres_network"
+  subscription_id = var.subscription
+  features {}
+}
+
 # Shared resource group (managed by separate pipeline)
 # Contains Key Vault and other shared resources
 data "azurerm_resource_group" "shared" {
---
+++ infrastructure/postgres.tf
@@ -0,0 +1,49 @@
+module "postgresql" {
+  providers = {
+    azurerm.postgres_network = azurerm.postgres_network
+  }
+
+  source        = "git@github.com:hmcts/terraform-module-postgresql-flexible?ref=master"
+  name          = "${var.product}-${var.env}"
+  env           = var.env
+  product       = var.product
+  component     = var.component
+  business_area = "cft"
+  common_tags   = var.common_tags
+
+  pgsql_databases = [
+    {
+      name = "expressjs-monorepo-template"
+    }
+  ]
+
+  pgsql_sku            = "GP_Standard_D2ds_v4"
+  pgsql_version        = "16"
+  pgsql_storage_mb     = 65536
+  auto_grow_enabled    = true
+  admin_user_object_id = var.ci_service_principal_object_id
+}
+
+resource "azurerm_key_vault_secret" "postgres_host" {
+  name         = "postgres-host"
+  value        = module.postgresql.fqdn
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "postgres_user" {
+  name         = "postgres-user"
+  value        = module.postgresql.username
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "postgres_password" {
+  name         = "postgres-password"
+  value        = module.postgresql.password
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
+
+resource "azurerm_key_vault_secret" "postgres_port" {
+  name         = "postgres-port"
+  value        = "5432"
+  key_vault_id = data.azurerm_key_vault.key_vault.id
+}
